14aaa9dee0820caf88c128fcdc452979b9a35a8c
caiman <me@caiman.dev>
2022-02-06 18:02:10 +0200

    [master] square 2

---
+++ scope.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+
+    scanf("%d", &size);
+
+    for ( int row = 1, counter = 1; row <= size; row++ ) {
+        for ( int col = 1; col < size; col++ ) {
+            printf("%d ", counter);
+            counter += 1;
+        }
+        printf("%d\n", counter);
+        counter += 1;
+    }
+
+    return 0;
+}
